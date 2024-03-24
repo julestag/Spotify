5a14b1633e4ef78908a66aa13383c091ec46f899
AnisElkt <151527107+AnisElkt@users.noreply.github.com>
2024-03-24 14:24:46 +0100

    Tracklist/Lecteur OK

--- AlbumD.js
+++ AlbumD.js
@@ -23,19 +23,25 @@ function AlbumDetails() {
 
 
     return ( // SI presence d'album affiche la div 
-        <div>
-            <h2>{album.name}</h2> {/* ?? */}
-
-            <p>Nombre de titre: {album.tracks.length}</p>
-            <ul>
-                {album.tracks.map(track => ( // map les Tracks a track pour sortir les clef name et sa durée
-                    <li key={track.id}>
-                        Titre - {track.name} - Durée: {track.duration} secondes
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+    <div>
+        <h2>{album.album.name}</h2> {/* ?? */}
+        <p>id de l'artiste : {album.album.artist_id}</p>
+    <p>{album.album.description}</p>
+    <img src={album.album.cover} alt={album.album.name} />
+    <p>date de sortie : {album.album.release_date}</p>
+    <p>Rangs de popularité : {album.album.popularity}</p>
+        <p>Nombre de titre: {album.tracks.length}</p>
+        <ul>
+            {album.tracks.map(track => ( // map les Tracks a track pour sortir les clef name et sa durée
+                <li key={track.id}>
+                <h4>{track.name}</h4>
+                <p>Durée : {track.duration} secondes</p>
+                <audio controls src={track.mp3}></audio>
+              </li>
+            ))}
+        </ul>
+    </div>
+);
 }
 
 export default AlbumDetails;
